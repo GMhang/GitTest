3a1cf080d50f65a508cfb334d25ec4086f0be0bf
GM <614396761@qq.com>
2022-12-08 22:31:21 +0800

    toGitHub commit

--- test01/src/main/java/com/gm/git/Main.java
+++ test01/src/main/java/com/gm/git/Main.java
@@ -9,5 +9,6 @@ package com.gm.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello GitHub commit!");
     }
 }
