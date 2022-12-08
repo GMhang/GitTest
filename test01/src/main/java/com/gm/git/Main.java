55d7ce8bf3c5e68a842bacaf59851a94080e2efc
GMhang <99591909+GMhang@users.noreply.github.com>
2022-12-08 22:36:52 +0800

    toIDEA commit

--- test01/src/main/java/com/gm/git/Main.java
+++ test01/src/main/java/com/gm/git/Main.java
@@ -10,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello GitHub commit!");
+        System.out.println("Hello IDEA commit!");
     }
 }
